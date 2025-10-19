cebe913ae3d3968f0b885671043fab699d57fbec
Mayar <Mayar>
2025-10-19 16:34:46 +0300

    01

--- Learning Journey/ContentView.swift
+++ Learning Journey/ContentView.swift
@@ -9,16 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+        
+             
+            
+       
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Text ("Hello Learner")
+                .font(.system(size: 34, weight: .bold))
+                .foregroundStyle(Color.white)
+            
+            Image("On")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150, height: 150)
+                
         }
-        .padding()
+        .padding(.horizontal,100)
+    }
+ 
+ 
     }
 }
-
 #Preview {
     ContentView()
 }
